868bb4bc89a9d3fda051189a5457afa2ac97880e
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-21 23:10:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 
-def second_reverse_each_word(sentence2)
-  sentence2 = "Hi again, just making sure it's reversed!"
-  sentence2.split.collect{|word| word.reverse}.join(" ")
+def second_reverse_each_word(sentence)
+  sentence = "Hi again, just making sure it's reversed!"
+  sentence.split.collect{|word| word.reverse}.join(" ")
 end
