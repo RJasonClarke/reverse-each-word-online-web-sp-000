aac44f8e0c3f1a3537fa32f57f48dded3b24c9c4
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-21 23:12:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,6 @@
 def reverse_each_word(sentence)
-  sentence = "Hello there, and how are you?"
-  sentence.split.collect{|word| word.reverse}.join(" ")
-end
-
-
-def reverse_each_word(sentence)
   sentence = "Hi again, just making sure it's reversed!"
   sentence.split.collect{|word| word.reverse}.join(" ")
 end
+
+reverse_each_word("Hello there, and how are you?")
