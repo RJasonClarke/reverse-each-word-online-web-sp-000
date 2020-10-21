f6152fb061a6cc28cff6f0fb12fee060bd76d0d5
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-21 23:10:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def first_reverse_each_word(array)
-
+def first_reverse_each_word(sentence1)
+  
 end
 
-def second_reverse_each_word(sentence)
-  sentence = "Hi again, just making sure it's reversed!"
-  sentence.split.collect{|word| word.reverse}.join(" ")
+def second_reverse_each_word(sentence2)
+  sentence2 = "Hi again, just making sure it's reversed!"
+  sentence2.split.collect{|word| word.reverse}.join(" ")
 end
