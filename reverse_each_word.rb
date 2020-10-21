f161ddfb48c5a3cc64835775ab8d63593681c00d
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-21 21:11:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def first_reverse_each_word(array)
   array = ["Hello there, and how are you?"]
-  array.map!{|element| element.reverse}array.join(" ")
+  array.map!do |element|
+  element.reverse
+  end
+array.join(" ")
 end
 
 def second_reverse_each_word(sentence)
