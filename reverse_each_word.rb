1872e7e8ca6be76730a5800e914ca382cb25031c
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-21 21:12:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def first_reverse_each_word(array)
   array = ["Hello there, and how are you?"]
-  array.map!do |element|
-  element.reverse
+  array.map! do |element|
+    element.reverse
   end
 array.join(" ")
 end
